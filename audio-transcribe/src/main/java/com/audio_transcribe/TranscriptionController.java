93ae8860b9521c95a7580b94b9ae3a5d9214b828
teo <teo.th@hotmail.gr>
2025-05-22 22:21:35 +0200

    initial commit with gitignore

---
+++ audio-transcribe/src/main/java/com/audio_transcribe/TranscriptionController.java
@@ -0,0 +1,12 @@
+package com.audio_transcribe;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transcribe")
+public class TranscriptionController {
+
+	
+	
+}
