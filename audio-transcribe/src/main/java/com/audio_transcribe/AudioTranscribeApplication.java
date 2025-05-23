8321e4d5bca270a953b154ab42e57c3f9374f2c3
teo <teo.th@hotmail.gr>
2025-05-23 18:15:01 +0200

    Load the .env in  main application class

--- audio-transcribe/src/main/java/com/audio_transcribe/AudioTranscribeApplication.java
+++ audio-transcribe/src/main/java/com/audio_transcribe/AudioTranscribeApplication.java
@@ -3,10 +3,14 @@ package com.audio_transcribe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AudioTranscribeApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // this reads the .env file
+        System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
 		SpringApplication.run(AudioTranscribeApplication.class, args);
 	}
 
